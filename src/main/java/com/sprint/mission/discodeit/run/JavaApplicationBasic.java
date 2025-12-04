2d908bed81648c62446d57194df33d02e0d518cb
홍성휘 <gkemg2017@gmail.com>
2025-12-04 22:11:39 +0900

    feat: test BasicApplication
    기존 JavaApplication을 Legacy로 전환하고, Basic Application을 새로 추가하여 테스트

---
+++ src/main/java/com/sprint/mission/discodeit/run/JavaApplicationBasic.java
@@ -0,0 +1,67 @@
+package com.sprint.mission.discodeit.run;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
+
+public class JavaApplicationBasic {
+    public static void main(String[] args) {
+
+        /* Basic + JCF 테스트 */
+        // Repository 주입
+        UserRepository userRepo = new JCFUserRepository();
+        ChannelRepository channelRepo = new JCFChannelRepository();
+        MessageRepository messageRepo = new JCFMessageRepository();
+
+        // Basic Service
+        UserService userServ = new BasicUserService(userRepo);
+        ChannelService channelServ = new BasicChannelService(channelRepo);
+        MessageService messageServ = new BasicMessageService(messageRepo);
+
+        // 테스트
+        User user = setupUser(userServ);
+        Channel channel = setupChannel(channelServ);
+        messageCreateTest(messageServ, channel, user);
+
+        /* Basic + File 테스트 */
+        UserService userServ2 = new BasicUserService(new FileUserRepository());
+        ChannelService channelServ2 = new BasicChannelService(new FileChannelRepository());
+        MessageService messageServ2 = new BasicMessageService(new FileMessageRepository());
+
+        User user2 = setupUser(userServ2);
+        Channel channel2 = setupChannel(channelServ2);
+        messageCreateTest(messageServ2, channel2, user2);
+
+    }
+
+    static User setupUser(UserService userService) {
+        User user = new User("홍길동", "gildong", "010-1234-5678", "password123", "abc@def.com", "gildong.jpg");
+        return userService.createUser(user);
+    }
+
+    static Channel setupChannel(ChannelService channelService) {
+        Channel channel = new Channel("공지", "공지 채널입니다");
+        return channelService.createChannel(channel);
+    }
+
+    static void messageCreateTest(MessageService messageService, Channel channel, User user) {
+        Message message = new Message(user.getId(), channel.getId(), "안녕하세요, 반갑습니다!");
+        messageService.createMessage(message);
+        System.out.println("메시지 생성 완료 : " + message.getId());
+    }
+}
--- src/main/java/com/sprint/mission/discodeit/run/JavaApplicationLegacy.java
+++ src/main/java/com/sprint/mission/discodeit/run/JavaApplicationLegacy.java
@@ -3,9 +3,18 @@ package com.sprint.mission.discodeit.run;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileMessageService;
 import com.sprint.mission.discodeit.service.file.FileUserService;
@@ -16,7 +25,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
-public class JavaApplication {
+public class JavaApplicationLegacy {
     public static void main(String[] args) {
 
         // 서비스 구현체 생성
