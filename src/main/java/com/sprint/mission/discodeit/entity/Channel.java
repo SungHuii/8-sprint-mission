7ebdff4a4ac8ab77f81ad6404f0667e652e5ceae
홍성휘 <gkemg2017@gmail.com>
2025-12-17 15:41:43 +0900

    feat: add enums ChannelType / modify Channel Entity
    채널 타입(Enums) 추가.
    채널 타입 추가에 따른 엔티티 필드 추가 및 메서드 수정

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.entity;
 
+import com.sprint.mission.discodeit.entity.enums.ChannelType;
 import lombok.Getter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -15,8 +17,10 @@ public class Channel implements Serializable {
     * 고유아이디
     * 생성시간
     * 수정시간
-    * 채널명
-    * 채널설명
+    * 채널명 public 채널 전용
+    * 채널설명 public 채널 전용
+    * 채널타입 (enum: PUBLIC, PRIVATE)
+    * Private 채널 전용
     * */
     @Serial
     private static final long serialVersionUID = 1L;
@@ -25,16 +29,33 @@ public class Channel implements Serializable {
     private Instant updatedAt;
     private String chName;
     private String chDescription;
+    private final ChannelType chType;
+    private final List<UUID> participantIds;
 
+    // PUBLIC 채널 생성자
     public Channel(String chName, String chDescription) {
         this.id = UUID.randomUUID();
         this.createdAt = Instant.now();
         this.updatedAt = this.createdAt;
+        this.chType = ChannelType.PUBLIC;
         this.chName = chName;
         this.chDescription = chDescription;
+        this.participantIds = List.of();
+    }
+
+    // PRIVATE 채널 생성자
+    public Channel(List<UUID> participantIds) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.updatedAt = this.createdAt;
+        this.chType = ChannelType.PRIVATE;
+        this.chName = null;
+        this.chDescription = null;
+        this.participantIds = validateParticipants(participantIds);
     }
 
     public void updateChName(String chName) {
+        validateChannelType();
         this.chName = chName;
         renewUpdatedAt();
     }
@@ -46,5 +67,18 @@ public class Channel implements Serializable {
 
     private void renewUpdatedAt() {
         this.updatedAt = Instant.now();
+    }
+
+    private static List<UUID> validateParticipants(List<UUID> ids) {
+        if (ids == null || ids.isEmpty()) {
+            throw new IllegalArgumentException("Private 채널은 참가자 목록이 필요합니다.");
+        }
+        return List.copyOf(ids);
+    }
+
+    private void validateChannelType() {
+        if (this.chType == ChannelType.PRIVATE) {
+            throw new IllegalStateException("PRIVATE 채널은 수정할 수 없습니다.");
+        }
     }
 }
