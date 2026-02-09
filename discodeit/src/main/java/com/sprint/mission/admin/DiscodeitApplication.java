d5d3cafb19c6867f61616c9ab45bce33d22f127b
홍성휘 <gkemg2017@gmail.com>
2026-02-09 22:18:59 +0900

    feat: add Spring Boot Admin metric dashboard
    심화 요구사항 : 메트릭 가시화 구현

--- discodeit/src/main/java/com/sprint/mission/admin/DiscodeitApplication.java
+++ discodeit/src/main/java/com/sprint/mission/admin/DiscodeitApplication.java
@@ -1,12 +1,15 @@
-package com.sprint.mission.discodeit;
+package com.sprint.mission.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class DiscodeitApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DiscodeitApplication.class, args);
   }
+
 }
